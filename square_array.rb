9eb4a89e1bcfb3aa1139dfca05f9599d05c628e3
Sashin Trout <s.ttrout@hotmail.com>
2020-08-09 15:01:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
 def square_array(array)
   # your code here
+  # created new array
   new_numbers = []
+  # manipulating array by passing num as an argument
   numbers.each do |num|
+    # appends new numbers to array
     new_numbers << (num ** 2)
+  # ends program 
   end
-  return 
+  # returns new value 
+  return new_numbers
 end
